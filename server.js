06406828729e2be4ef7df4cecf8ab172ce858c2b
Caltjuh <calvinjohn2002@hotmail.nl>
2023-04-25 16:26:42 +0200

    Back-end les 2

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/', onhome)
+  .get('/about', onabout)
+
+
+function onhome(req, res) {
+  res.send('<h1>Hello World!<h1>')
+}
+
+function onabout(req,res) {
+  res.send('<h1>about me</h1>')
+}
